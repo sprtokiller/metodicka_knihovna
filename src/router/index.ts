863caff716cf7d972b6c0c57a2b7f77c0a42cc63
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2023-06-02 12:33:07 +0200

    UNSAFE DEV ONLY FIX - changed router to not use lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+/* BEGIN DEV ONLY */
+import HomeView from '../views/HomeView.vue'
+import ActivityView from '../views/ActivityView.vue'
+/* END DEV ONLY */
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      // component: HomeView
-      component: () => import('../views/ActivityView.vue')
+      name: 'activity',
+      /* BEGIN DEV ONLY */
+      component: ActivityView
+      /* END DEV ONLY */
+      /* BEGIN PROD ONLY */
+      // component: () => import('../views/ActivityView.vue')
+      /* END PROD ONLY */
     },
     {
       path: '/tutorial',
       name: 'tutorial',
-      component: () => import('../views/HomeView.vue')
+      /* BEGIN DEV ONLY */
+      component: HomeView
+      /* END DEV ONLY */
+      /* BEGIN PROD ONLY */
+      // component: () => import('../views/HomeView.vue')
+      /* END PROD ONLY */
     }
   ]
 })
