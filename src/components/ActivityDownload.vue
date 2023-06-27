b2032572a432547f99e0590539c33982f3e99f09
KoMatrich <kocichmartin@gmail.com>
2023-06-27 21:58:46 +0200

    More pages done

--- src/components/ActivityDownload.vue
+++ src/components/ActivityDownload.vue
@@ -1,10 +1,20 @@
 <template>
   <section>
     <n-h2 prefix="bar">Ke stažení</n-h2>
-    TODO
+    <div class="my-5">
+      <n-card title="Pracovní list">
+        TODO - add space between buttons
+        <br />
+        <n-button dashed>Pro novinářskou redakci / tým expertů</n-button>
+        <br />
+        <n-button dashed
+          >Přehled témat pro tvorbu otázek jednotlivými novinářskými redakcemi</n-button
+        >
+      </n-card>
+    </div>
   </section>
 </template>
-  
+
 <script setup lang="ts">
-import { NH2 } from 'naive-ui';
+import { NH2, NCard, NButton } from 'naive-ui'
 </script>
--- src/components/ActivityQuestions.vue
+++ src/components/ActivityQuestions.vue
@@ -1,10 +1,37 @@
 <template>
   <section>
     <n-h2 prefix="bar">Návodové otázky</n-h2>
-    TODO
+    <div class="my-5">
+      <n-card title="Mám nastavit nějaká pravidla pro vymýšlení názvů jednotlivých redakcí?"
+        >Chytrá odpověď
+      </n-card>
+    </div>
+    <div class="my-5">
+      <n-card title="Do jak velkých skupin mám třídu rozdělit?">Chytrá odpověď </n-card>
+    </div>
+    <div class="my-5">
+      <n-card title="Kolik skupin bude vystupovat v roli expertů?">Chytrá odpověď </n-card>
+    </div>
+    <div class="my-5">
+      <n-card title="Mám zadat téma pro TK všem skupinám naráz nebo postupně?"
+        >Chytrá odpověď
+      </n-card>
+    </div>
+    <div class="my-5">
+      <n-card title="Jaký časový limit mám nastavit na generování 5 otázek?"
+        >Chytrá odpověď
+      </n-card>
+    </div>
+    <div class="my-5">
+      <n-card title="Za expertní tým má mluvit jen jeden student nebo se mohou střídat?"
+        >Chytrá odpověď
+      </n-card>
+    </div>
+    <div class="my-5">
+      <n-card title="Jak mám posoudit, zda byla odpověď správná nebo ne?">Chytrá odpověď </n-card>
+    </div>
   </section>
 </template>
-  
 <script setup lang="ts">
-import { NH2 } from 'naive-ui';
+import { NH2, NCard } from 'naive-ui'
 </script>
