6b808a788b1363078abc7a5cfbaab209fec00b26
KoMatrich <kocichmartin@gmail.com>
2023-06-29 18:53:07 +0200

    Better spacing

--- src/components/ActivityDownload.vue
+++ src/components/ActivityDownload.vue
@@ -1,20 +1,20 @@
 <template>
   <section>
     <n-h2 prefix="bar">Ke stažení</n-h2>
-    <div class="my-5">
-      <n-card title="Pracovní list">
-        TODO - add space between buttons
-        <br />
-        <n-button dashed>Pro novinářskou redakci / tým expertů</n-button>
-        <br />
-        <n-button dashed
-          >Přehled témat pro tvorbu otázek jednotlivými novinářskými redakcemi</n-button
-        >
-      </n-card>
-    </div>
+    <n-card title="Pracovní list">
+      <n-button dashed>Pro novinářskou redakci / tým expertů</n-button>
+      <br />
+      <n-button dashed>Přehled témat pro tvorbu otázek jednotlivými novinářskými redakcemi</n-button>
+    </n-card>
   </section>
 </template>
 
 <script setup lang="ts">
 import { NH2, NCard, NButton } from 'naive-ui'
 </script>
+
+<style scoped>
+.n-button {
+  margin-bottom: 1rem;
+}
+</style>
--- src/components/ActivityQuestions.vue
+++ src/components/ActivityQuestions.vue
@@ -1,37 +1,25 @@
 <template>
   <section>
     <n-h2 prefix="bar">Návodové otázky</n-h2>
-    <div class="my-5">
-      <n-card title="Mám nastavit nějaká pravidla pro vymýšlení názvů jednotlivých redakcí?"
-        >Chytrá odpověď
-      </n-card>
-    </div>
-    <div class="my-5">
-      <n-card title="Do jak velkých skupin mám třídu rozdělit?">Chytrá odpověď </n-card>
-    </div>
-    <div class="my-5">
-      <n-card title="Kolik skupin bude vystupovat v roli expertů?">Chytrá odpověď </n-card>
-    </div>
-    <div class="my-5">
-      <n-card title="Mám zadat téma pro TK všem skupinám naráz nebo postupně?"
-        >Chytrá odpověď
-      </n-card>
-    </div>
-    <div class="my-5">
-      <n-card title="Jaký časový limit mám nastavit na generování 5 otázek?"
-        >Chytrá odpověď
-      </n-card>
-    </div>
-    <div class="my-5">
-      <n-card title="Za expertní tým má mluvit jen jeden student nebo se mohou střídat?"
-        >Chytrá odpověď
-      </n-card>
-    </div>
-    <div class="my-5">
-      <n-card title="Jak mám posoudit, zda byla odpověď správná nebo ne?">Chytrá odpověď </n-card>
-    </div>
+    <n-card title="Mám nastavit nějaká pravidla pro vymýšlení názvů jednotlivých redakcí?">Chytrá odpověď
+    </n-card>
+    <n-card title="Do jak velkých skupin mám třídu rozdělit?">Chytrá odpověď </n-card>
+    <n-card title="Kolik skupin bude vystupovat v roli expertů?">Chytrá odpověď </n-card>
+    <n-card title="Mám zadat téma pro TK všem skupinám naráz nebo postupně?">Chytrá odpověď
+    </n-card>
+    <n-card title="Jaký časový limit mám nastavit na generování 5 otázek?">Chytrá odpověď
+    </n-card>
+    <n-card title="Za expertní tým má mluvit jen jeden student nebo se mohou střídat?">Chytrá odpověď
+    </n-card>
+    <n-card title="Jak mám posoudit, zda byla odpověď správná nebo ne?">Chytrá odpověď </n-card>
   </section>
 </template>
 <script setup lang="ts">
 import { NH2, NCard } from 'naive-ui'
 </script>
+
+<style scoped>
+.n-card {
+  margin-bottom: 1rem;
+}
+</style>
